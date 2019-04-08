27b010869417236af6bffb1bb36485842a749fa8
EunBeenKim <gichanac@naver.com>
2019-04-08 21:30:47 +0900

    적 기본 AI 작성
    <추가된 파일>
    EnemyAIBase.cs, EnemyAnimalAI.cs, EnemyQuarryAI.cs, EnemyTribeAI.cs 작성했음.
    NavMeshComponents 폴더는 NPC들이 알아서 길찾기 위해서 추가한 파일입니다.
    
    <설명>
    기본 베이스가 되는 적 AI(EnemyAIBase)로부터 상속받아 자식 클래스를 작성하였음.
    자식 클래스에선 부모 클래스의 변수와 메서드를 사용할 수 있어서 기존 메서드를 덮어씌우거나(Override) 자식 클래스만의 스크립트를 작성할수있음.
    
    부모 클래스의 FSM은 자식클래스에도 존재해서 상태를 계속해서 전환시켜준다. state = "???" 만 해주면 알아서 바뀐단 말씀!
    모르겠으면 질문하세옹.
    예진이는 참고영상 봐야 스크립트 작성하는데 도움될거야
    
    <참고>
    - NavMesh 사용 튜토리얼
    https://www.youtube.com/watch?v=CHV1ymlw-P8&list=LLWmX6VF12_IBEd-tqTKjHug&index=3&t=0s
    - FSM을 이용한 기본 AI 만들기
    https://www.youtube.com/watch?v=s67AYDD3j1E&list=LLWmX6VF12_IBEd-tqTKjHug&index=3
    - 유니티 상속과 상태변화(FSM)
    http://blog.naver.com/PostView.nhn?blogId=mrbinggrae&logNo=220508805926

---
+++ a Tribe without Words/Assets/Script/EnemyAIBase.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// 공통적인 적 AI 설계. 
+// 상속시킬 변수, 메서드 정의.
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAIBase : MonoBehaviour
+{
+    public enum State
+    {
+        PATROL,
+        CHASE,
+        ATTACK,
+        RUNAWAY,
+        DIE
+    }
+
+    public NavMeshAgent agent;
+    //public CharacterController charContrler; /* CharacterController외에 다른 컨트롤러 컴포넌트가 있다면 그것을 쓰자 */
+
+    // AI 상태 변수
+    public State state;
+    protected bool alive;
+
+    // 모든 개체들은 아래와 같은 과정을 거친다.
+    public virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //charContrler = GetComponent<CharacterController>();
+
+        /* updateRotation 수정해야함? 일단 보류 */
+        agent.updatePosition = true;
+        agent.updateRotation = true;
+
+        state = EnemyAIBase.State.PATROL;
+        alive = true;
+
+        StartCoroutine("FSM");
+    }
+
+    // alive가 true인 상태동안 무한 반복
+    IEnumerator FSM()
+    {
+        while (alive)
+        {
+            switch (state)
+            {
+                case State.PATROL:
+                    Patrol();
+                    break;
+                case State.CHASE:
+                    Chase();
+                    break;
+                case State.ATTACK:
+                    Attack();
+                    break;
+                case State.RUNAWAY:
+                    Runaway();
+                    break;
+                case State.DIE:
+                    Die();
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    virtual protected void Patrol()
+    {
+        Debug.Log("자식 스크립트에서 함수가 호출되어져야 한다.");
+    }
+
+    virtual protected void Chase()
+    {
+
+        Debug.Log("자식 스크립트에서 함수가 호출되어져야 한다.");
+    }
+
+    virtual protected void Attack()
+    {
+        Debug.Log("자식 스크립트에서 함수가 호출되어져야 한다.");
+    }
+
+    virtual protected void Runaway()
+    {
+        Debug.Log("자식 스크립트에서 함수가 호출되어져야 한다.");
+    }
+
+    virtual protected void Die()
+    {
+        alive = false;
+    }
+}
---
+++ a Tribe without Words/Assets/Script/EnemyTribeAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 공격하는 적 부족
+public class EnemyTribeAI : EnemyAIBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
