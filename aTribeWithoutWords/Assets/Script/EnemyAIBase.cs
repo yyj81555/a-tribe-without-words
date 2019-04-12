3d90070242ad936f865cd2c0f935e1de7f0a4a48
EunBeenKim <gichanac@naver.com>
2019-04-12 17:02:32 +0900

    유니티 버전 변경 (17.4.25 LTS)

---
+++ aTribeWithoutWords/Assets/Script/EnemyAIBase.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+// 공통적인 적 AI 설계. 
+// 상속시킬 변수, 메서드 정의.
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyAIBase : MonoBehaviour
+{
+    public enum State
+    {
+        PATROL,
+        CHASE,
+        ATTACK,
+        RUNAWAY,
+        DIE
+    }
+
+    public NavMeshAgent agent;
+    // public ThirdPersonCharacter charContrler;
+    // ThirdPersonCharacter를 활용하면 agent의 이동에 따라 변하는 회전, 속도 등을 고려한 자연스러운 애니메이션을 구현가능.
+    // 이것을 직접 구현하려면 꽤 어려움. ThirdPersonCharacter를 분석해서 수정하는것이 빠를거같음.
+
+    // AI 상태 변수
+    public State state;
+    protected bool alive;
+
+    // 모든 개체들은 아래와 같은 과정을 거친다.
+    public virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //charContrler = GetComponent<ThirdPersonCharacter>();
+
+        // updateRotation = true 하게 되면 agent가 목표위치로 움직이면서 회전하는 것을 반영하게 됨. -> 여기에 단순히 애니메이션을 씌우게 되면 이상하게 움직임.
+        // updateRotation = false로 하고 ThirdPersonCharacter.cs 에서 작성된 자체 계산 함수(Move)를 이용하도록 한다.
+        agent.updatePosition = true;
+        agent.updateRotation = false;
+
+        state = EnemyAIBase.State.PATROL;
+        alive = true;
+
+        StartCoroutine("FSM");
+    }
+
+    // alive가 true인 상태동안 무한 반복
+    IEnumerator FSM()
+    {
+        while (alive)
+        {
+            switch (state)
+            {
+                case State.PATROL:
+                    Patrol();
+                    break;
+                case State.CHASE:
+                    Chase();
+                    break;
+                case State.ATTACK:
+                    Attack();
+                    break;
+                case State.RUNAWAY:
+                    Runaway();
+                    break;
+                case State.DIE:
+                    Die();
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    virtual protected void Patrol()
+    {
+        Debug.Log("자식 스크립트에서 함수가 호출되어져야 한다.");
+    }
+
+    virtual protected void Chase()
+    {
+
+        Debug.Log("자식 스크립트에서 함수가 호출되어져야 한다.");
+    }
+
+    virtual protected void Attack()
+    {
+        Debug.Log("자식 스크립트에서 함수가 호출되어져야 한다.");
+    }
+
+    virtual protected void Runaway()
+    {
+        Debug.Log("자식 스크립트에서 함수가 호출되어져야 한다.");
+    }
+
+    virtual protected void Die()
+    {
+        alive = false;
+    }
+}
---
+++ aTribeWithoutWords/Assets/Script/EnemyAnimalAI.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 공격하는 동물
+public class EnemyAnimalAI : EnemyAIBase
+{
+    // EnemyAIBase로 부터 상속받는 변수 확인할 것.
+    // ...
+
+    [Header("State Setting")]
+    public int hp = 10;
+
+    // Var for patrol
+    public GameObject[] waypoints;  // 정찰하는 지점
+    private int waypointIndex = 0;
+    public float patrolSpeed = 3f;
+    float waypntLeftDist = 4f; // waypoint까지 남은 거리
+
+    // Var for chase
+    public float chaseSpeed = 5f;
+    private GameObject target;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Patrol()
+    {
+        agent.speed = patrolSpeed;
+
+        if (Vector3.Distance(this.transform.position, waypoints[waypointIndex].transform.position) >= waypntLeftDist)
+        {
+            agent.SetDestination(waypoints[waypointIndex].transform.position);
+        }
+        // waypoint에 도달한 경우
+        else if(Vector3.Distance(this.transform.position, waypoints[waypointIndex].transform.position) < waypntLeftDist)
+        {
+            waypointIndex += 1; // 다음 지점으로 이동
+            if(waypointIndex >= waypoints.Length)
+            {
+                waypointIndex = 0;
+            }
+            Debug.Log("다음 지점으로 이동");
+        }
+        else
+        {
+            Debug.Log("???");
+        }
+    }
+
+    protected override void Chase()
+    {
+        agent.speed = chaseSpeed;
+        agent.SetDestination(target.transform.position);
+
+        // 타겟의 주변에 도착했다면
+        if(Vector3.Distance(this.transform.position, target.transform.position) <= 3f)
+        {
+            state = State.ATTACK;
+        }
+    }
+
+    protected override void Attack()
+    {
+
+        // 타겟이 
+        if (Vector3.Distance(this.transform.position, target.transform.position) > 3f)
+        {
+            state = State.CHASE;
+        }
+    }
+
+    protected override void Die()
+    {
+        base.Die();
+        Destroy(this.gameObject);
+    }
+
+    public void AttackedByWorker()
+    {
+
+    }
+
+    // worker를 탐지하면 쫓아가도록 테스트.
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "worker")
+        {
+            // 추격하는 중에는 다른 개체를 추격하지 않는다.
+            if (state != State.CHASE)
+            {
+                target = other.gameObject;
+                state = State.CHASE;
+                Debug.Log("잡아라!");
+            }
+        }
+    }
+
+    // 탐지영역에서 사라지면 다시 탐색
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "worker")
+        {
+            // 탐지영역에서 사라진 개체가 target이라면
+            if (target == other.gameObject)
+                state = State.PATROL;
+        }
+    }
+}
