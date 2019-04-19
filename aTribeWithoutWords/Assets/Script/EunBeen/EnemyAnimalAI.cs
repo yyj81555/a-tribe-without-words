f375fbd905eef6a739347d537f682c10f9ce5af8
EunBeenKim <gichanac@naver.com>
2019-04-19 18:07:46 +0900

    프로젝트 임시로 합침

--- aTribeWithoutWords/Assets/Script/EunBeen/EnemyAnimalAI.cs
+++ aTribeWithoutWords/Assets/Script/EunBeen/EnemyAnimalAI.cs
@@ -1,61 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-// 공격하는 적 부족(일꾼 공격, 방해)
-public class EnemyTribeAI : EnemyAIBase
+// 방해하는 동물(식량 탈취)
+public class EnemyAnimalAI : EnemyAIBase
 {
     // EnemyAIBase로 부터 상속받는 변수 확인할 것.
     // ...
 
-    public enum EnmyTribeType
-    {
-        SOLDIER,  // 전사
-        SHAMAN    // 주술사
-    }
-
     [Header("State Setting")]
-    public int hp = 20;
-    public EnmyTribeType tribeType = EnmyTribeType.SOLDIER;  // 적 부족 종류
-
+    public int hp = 10;
 
     // 정찰에 대한 변수
     public GameObject[] waypoints;  // 정찰하는 지점
     private int waypointIndex = 0;
-    public float patrolSpeed = 2f;
-    const float waypntLeftDist = 4f; // waypoint까지 남은 거리
+    public float patrolSpeed = 3f;
+    float waypntLeftDist = 4f; // waypoint까지 남은 거리
 
     // 추격에 대한 변수
-    public float chaseSpeed = 4f;
+    public float chaseSpeed = 5f;
     private List<GameObject> targets;
 
     // 공격에 대한 변수
-    public float attackPower = 3;
-    private float attackTime = 0f;
-    private float attackCycleTime;  // 공격 사이클
-    const float attackRange = 8f;   // 공격 사정거리
+    public float attackPower = 1;
+    float attackTime = 0f;
+    float attackRange = 3f;
 
     public override void Start()
     {
         base.Start();
 
         targets = new List<GameObject>();
-
-        // 적 부족타입에 따라 다르게 처리
-        switch (tribeType)
-        {
-            case EnmyTribeType.SOLDIER:
-                hp = 10;
-                attackCycleTime = 2f;
-                break;
-            case EnmyTribeType.SHAMAN:
-                hp = 20;
-                attackCycleTime = 4f;
-                break;
-        }
     }
-    #region AI 행동들
+
     protected override void Patrol()
     {
         agent.speed = patrolSpeed;
@@ -64,13 +41,12 @@ public class EnemyTribeAI : EnemyAIBase
         {
             agent.SetDestination(waypoints[waypointIndex].transform.position);
             LookToward(waypoints[waypointIndex].transform.position);
-            Debug.Log("정찰중");
         }
         // waypoint에 도달한 경우
-        else if (Vector3.Distance(this.transform.position, waypoints[waypointIndex].transform.position) < waypntLeftDist)
+        else if(Vector3.Distance(this.transform.position, waypoints[waypointIndex].transform.position) < waypntLeftDist)
         {
             waypointIndex += 1; // 다음 지점으로 이동
-            if (waypointIndex >= waypoints.Length)
+            if(waypointIndex >= waypoints.Length)
             {
                 waypointIndex = 0;
             }
@@ -91,42 +67,26 @@ public class EnemyTribeAI : EnemyAIBase
         // 공격 사정거리에 달했다면 공격한다.
         if (Vector3.Distance(this.transform.position, targets[0].transform.position) <= attackRange)
         {
-            Debug.Log("Target Close");
             state = State.ATTACK;
         }
     }
 
+    // 일꾼을 방해한다.
     protected override void Attack()
     {
         // 공격하기에 너무 멀다면 다시 쫓아간다. (탐지영역을 벗어날 정도로 멀면 OnTriggerExit에서 정찰상태가 된다.)
         if (Vector3.Distance(this.transform.position, targets[0].transform.position) > attackRange)
         {
-            ResumeMove();
             state = State.CHASE;
-            Debug.Log("Target Too Far");
         }
 
-        // 멈춰서 공격
-        //agent.SetDestination(transform.position); /* 임시방편으로 했는데 잘됨 ? */
-        PauseMove();
-        LookToward(targets[0].transform.position);
-
         attackTime += Time.deltaTime;
-        if (attackTime > attackCycleTime) // 2초마다 공격
+        if(attackTime > 3f) // 3초마다 공격
         {
+            Debug.Log("worker 방해");
             attackTime = 0.0f;
             // target.GetComponent<???>().workerHp -= attackPower;
-            // 위와같이 공격받았을때 호출되는 함수 호출
-
-            // 부족 타입에 따라 다른 공격 처리
-            if (tribeType == EnmyTribeType.SOLDIER)
-            {
-                Attack_Soldier();
-            }
-            else if (tribeType == EnmyTribeType.SHAMAN)
-            {
-                Attack_Shaman();
-            }
+            // 혹은 공격받았을때 호출되는 함수 호출
         }
     }
 
@@ -135,7 +95,6 @@ public class EnemyTribeAI : EnemyAIBase
         base.Die();
         Destroy(this.gameObject);
     }
-    #endregion
 
     public void AttackedByWorker()
     {
@@ -143,13 +102,12 @@ public class EnemyTribeAI : EnemyAIBase
     }
 
     /*Enter와 Exit에서 유의할점은 해당 이벤트가 발생할때만 아래 처리를 한다는 것이다.
-      만일 콜라이더내에 머무는 동안 Enemy가 공격하여 worker가 사라진다면 Exit이벤트는 발생하지 않는다.
-      worker가 공격받다 죽었을때 취하는 함수가 필요함. */
+       만일 콜라이더내에 머무는 동안 Enemy가 공격하여 worker가 사라진다면 Exit이벤트는 발생하지 않는다.*/
 
     // detector콜라이더에 worker가 탐지되면 쫓아감.
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "worker")
+        if(other.tag == "Worker")
         {
             // 타겟리스트에 저장되어 있지 않다면 저장
             if (!(targets.Contains(other.gameObject)))
@@ -163,7 +121,7 @@ public class EnemyTribeAI : EnemyAIBase
     // 탐지영역에서 사라지면 다시 탐색
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "worker")
+        if(other.tag == "Worker")
         {
             // 타겟 리스트에 포함되어 있다면 타겟에서 제외한다.
             if (targets.Contains(other.gameObject))
@@ -171,24 +129,12 @@ public class EnemyTribeAI : EnemyAIBase
                 targets.Remove(other.gameObject);
 
                 // 남아있는 타겟이 없다면 정찰상태로 변경
-                // 남아있는 타겟이 있다면 자동으로 추격할 것이다. target[0]에 Queue처럼 새로운 놈이 들어감.
-                Debug.Log("" + targets.Count);
+                // 남아있는 타겟이 있었다면 자동으로 추격할 것이다. target[0]에 Queue처럼 새로운 놈이 들어감.
                 if (targets.Count == 0)
                 {
                     state = State.PATROL;
-                    Debug.Log("Patrol 상태 전환");
                 }
             }
         }
-    }
-
-    private void Attack_Soldier()
-    {
-        Debug.Log("전사 공격");
-    }
-
-    private void Attack_Shaman()
-    {
-        Debug.Log("샤먼 공격");
     }
 }
---
+++ aTribeWithoutWords/Assets/Script/YeJin/RayCast.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+
+public class RayCast : MonoBehaviour
+{
+    //rayCast
+    [SerializeField]
+    private float rayLength;
+
+    public string hitname = null;
+
+    Variable variable;
+    NPCMove npcmove;
+
+    public GameObject prefab;
+
+    public int ChooseCount = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        variable = GameObject.Find("ItemList").GetComponent<Variable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, rayLength))
+            {
+                Debug.Log(hit.collider.name);
+                hitname = hit.collider.name;
+
+                if (hit.transform.gameObject.tag == "Worker")
+                {
+                    if (GameObject.Find(hitname + "Effect") == null && variable.EffectCount < ChooseCount)
+                    {
+                      GameObject obj = Instantiate(prefab, new Vector3(hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y + 2, hit.collider.gameObject.transform.position.z), Quaternion.identity);
+                      obj.name = hitname + "Effect";
+                      npcmove = GameObject.Find(hitname).GetComponent<NPCMove>();
+                      npcmove.RePos();
+                    }
+                }
+
+                else if(hit.transform.gameObject.tag == "FruitFarm")
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+
+                        npcmove = GameObject.Find("Worker" + i).GetComponent<NPCMove>();
+
+                        if (variable.chooseNPC[i] == 1 && npcmove.NPCState == 1)
+                        {
+                            npcmove.CommandState = 1;
+                            npcmove.IndicationPos();
+                        }
+                    }
+                }
+                    
+                else if (hit.transform.gameObject.tag == "Mine")
+                {
+                    if (variable.EffectCount >= 2)
+                    {
+                        Debug.Log("수행인원이 너무 많습니다.");
+
+                        for (int i = 1; i <= 3; i++)
+                        {
+                            npcmove = GameObject.Find("Worker" + i).GetComponent<NPCMove>();
+                            npcmove.standardMode();
+                            Destroy(GameObject.Find("Worker" + i + "Effect"));
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 1; i <= 3; i++)
+                        {
+                            npcmove = GameObject.Find("Worker" + i).GetComponent<NPCMove>();
+
+                            if (variable.chooseNPC[i] == 1 && npcmove.NPCState == 1)
+                            {
+                                npcmove.CommandState = 2;
+                                npcmove.IndicationPos();
+                            }
+                        }
+                    }
+                }   
+            } 
+        }
+    }
+}
